e0abb0802c0b84f9f1c4eaa3023e4e3c0b977a10
Shardul1908 <sharduld80@gmail.com>
2021-05-27 17:57:01 +0530

    Assignment2:Corrections1

--- Assignment_2/src/com/company/PassTwo/PassTwo.java
+++ Assignment_2/src/com/company/PassTwo/PassTwo.java
@@ -38,18 +38,26 @@ public class PassTwo {
             String[] parts = line.split("\\s+");
             if(parts[0].contains("AD") || parts[0].equals("(DL,01)")) {
                 sb.append("__ __ ____");
+
             }else if(parts.length == 1) {
-//                if(parts[0].contains("IS")) {
-                    sb.append(getCode(parts[0])).append(" 00 0000");
-//                }
+                sb.append(getCode(parts[0])).append(" 00 0000");
+
             }else if(parts.length == 2) {
+
                 if(parts[0].contains("IS")) {
                     int i = Integer.parseInt(getCode(parts[1]));
-                    sb.append(getCode(parts[0])).append(" 00").append(" ").append(symbolTable.get(i-1).getAddress());
+                    if(parts[1].contains("L")) {
+                        sb.append(getCode(parts[0])).append(" 00").append(" ").append(literalTable.get(i-1).getAddress());
+                    }
+                    else if(parts[1].contains("S")) {
+                        sb.append(getCode(parts[0])).append(" 00").append(" ").append(symbolTable.get(i-1).getAddress());
+                    }
                 }else if(parts[0].equals("(DL,02)")) {
                     sb.append("00 00 000").append(getCode(parts[1]));
                 }
+
             }else {
+
                 int i = Integer.parseInt(getCode(parts[2]));
                 sb.append(getCode(parts[0])).append(" ").append(getCode(parts[1])).append(" ");
                 if(parts[2].contains("L")) {
@@ -58,12 +66,12 @@ public class PassTwo {
                 else if(parts[2].contains("S")) {
                     sb.append(symbolTable.get(i-1).getAddress());
                 }
+
             }
             sb.append("\n");
         }
         return sb.toString();
     }
-
 
     private String getCode(String s) {
         String[] parts = s.split(",");
